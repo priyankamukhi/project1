6e261ee99415fa77112d7f6ce2e981baa26864a0
Priyanka_Mukhi <priyankamukhi96@gmail.com>
2021-12-23 18:52:35 +0530

    new project

---
+++ project/Controllers/staffController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using project.Models;
+
+
+namespace project.Controllers
+{
+    public class staffController : ApiController
+    {
+        IList<student> StudentList = new List<student>()
+       {
+           new student (){stdid=1,stdname="ankit",stdclass=12,stdage=18,stdcontactno=700863432},
+           new student (){stdid=2,stdname="anuj",stdclass=12,stdage=18,stdcontactno=700678976},
+           new student (){stdid=3,stdname="rahul",stdclass=12,stdage=18,stdcontactno=985963432},
+           new student (){stdid=4,stdname="muchkund",stdclass=12,stdage=18,stdcontactno=600863432}
+        };
+        public ActionResult index()
+
+        {
+            return view(StudentList);
+        }
+    }
+}
---
+++ project/Models/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project.Models
+{
+    public class student
+    {
+        public int stdid { get; set; }
+        public string stdname{ get; set; }
+        public int stdclass { get; set; }
+        public int stdage { get; set; }
+        public int stdcontactno {get; set; }
+    }
+}
